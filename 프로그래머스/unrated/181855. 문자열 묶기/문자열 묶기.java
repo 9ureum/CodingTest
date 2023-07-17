db6489511bffb8c3115e6a208b5be6ee8ef2b3d0
9ureum <127267102+9ureum@users.noreply.github.com>
2023-07-17 21:03:15 +0900

    [unrated] Title: 문자열 묶기, Time: 31.35 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        for (int i = 1 ; i <= 30 ; i++) {
+            int cnt = 0;
+            for (int j = 0; j < strArr.length; j++) {
+                if (strArr[j].length() == i) {
+                    cnt++;
+                }
+                answer = Math.max(cnt,answer);
+            }
+        }
+        return answer;
+    }
+}
