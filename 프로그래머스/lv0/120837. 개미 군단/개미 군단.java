c0021ed29e66acab399dc6474d65217d51dafd00
9ureum <127267102+9ureum@users.noreply.github.com>
2023-08-25 14:27:22 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,14 +1,8 @@
 class Solution {
-    //장군개미 : 5, 병정개미 : 3, 일개미 : 1
+    
     public int solution(int hp) {
         int answer = 0;
-        int janggun = 5;
-        int byeongjung = 3;
-        int gaemi = 1;
-        
-        answer = (hp / janggun) + (hp - (hp / janggun) * 5) / byeongjung 
-            + (hp - (hp / janggun) * 5) - ((hp - (hp / janggun) * 5)/ byeongjung) * 3;
-        
+        answer = hp / 5 + (hp % 5) / 3 + hp % 5 % 3;
         return answer;
     }
 }
