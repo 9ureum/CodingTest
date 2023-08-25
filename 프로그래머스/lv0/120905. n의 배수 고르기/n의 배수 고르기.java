ec4335011c5d6fbfceda03e7e976c5af79b1e2ef
9ureum <127267102+9ureum@users.noreply.github.com>
2023-08-25 13:55:40 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -5,18 +5,14 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         
         for (int num : numlist) {
+            if (num % n == 0) {
             list.add(num);
-        }
-        for (int number : numlist) {
-            if (number % n != 0) {
-                list.remove((Integer)number);
             }
         }
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++){
             answer[i] = list.get(i);
         }
-        
         return answer;
     }
 }
