61c3d7f51c152cb99f8212e103b4489ad56508e1
9ureum <127267102+9ureum@users.noreply.github.com>
2023-08-28 23:20:54 +0900

    [level 1] Title: 폰켓몬, Time: 3.69 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -2,9 +2,9 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        Map<Integer, Integer> ponketmon = new HashMap<>();
+        Set<Integer> ponketmon = new HashSet<>();
         for(int i = 0; i < nums.length; i++) {
-            ponketmon.put(nums[i],nums[i]);
+            ponketmon.add(nums[i]);
         }
         if (nums.length/2 < ponketmon.size()) {
             answer = nums.length/2;
