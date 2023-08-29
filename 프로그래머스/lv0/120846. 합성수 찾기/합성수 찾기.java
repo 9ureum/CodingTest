d60f27e9943db85dc75bd2af52fe52272ec36a98
9ureum <127267102+9ureum@users.noreply.github.com>
2023-08-29 09:01:35 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -4,18 +4,18 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             int cnt = 0;
             for(int j = 1; j <= i; j++){
-if (i % j == 0) {
-cnt++;
-} else {
-continue;
-}
-                if(cnt>=3) {
-answer++;
-                    break;
-} else {
+                if (i % j == 0) {
+                    cnt++;
+                } else {
                     continue;
                 }
-}
+                if(cnt>=3) {
+                    answer++;
+                    break;
+                } else {
+                    continue;
+                }
+            }
             
         }
         return answer;
