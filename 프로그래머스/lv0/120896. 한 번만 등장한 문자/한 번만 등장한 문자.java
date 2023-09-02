82e091e64d605618788966edec11ae321d36e589
9ureum <127267102+9ureum@users.noreply.github.com>
2023-09-02 19:25:15 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.58 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -2,20 +2,19 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] str = s.split("");
-        Arrays.sort(str);
-        for (int i = 0; i < str.length; i++) {
-            int cnt = 0;
-            for (int j = 0; j < str.length; j++) {
-                if (str[i].equals(str[j])) {
-                    cnt++;
-                }
-            }
-            if (cnt == 1) {
-                answer += str[i];
+        Map<String, Integer> m = new HashMap<>();
+        String[] strarr = s.split("");
+        Arrays.sort(strarr);
+        for (String str : strarr) {
+            m.put(str, m.getOrDefault(str,0) + 1);
+        }
+        for (String ss : m.keySet()) {
+            if (m.get(ss) == 1) {
+                answer += ss;
+            } else {
+                continue;
             }
         }
-    
         return answer;
     }
 }
